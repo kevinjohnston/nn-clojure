0a6ca5f74ac775633de17b3328b3eb7d9a08b521
Kevin Johnston <kevinjohnston512@gmail.com>
2020-06-22 19:53:25 -0500

    Add higher level domain query functions

--- src/nn_clojure/domain.clj
+++ src/nn_clojure/domain.clj
@@ -69,3 +69,47 @@
    :post [(vex :delta/layer %)]}
   (-> ctx :delta/nn (nth (shift i (num-deltas ctx) (num-layers ctx)))))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;; Domain queries -- higher level questions about ctx
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defn- num-layer-inputs
+  "Return the number of inputs to a neuron in the given layer."
+  [ctx i]
+  (let [any-neuron first]
+    (-> ctx (layer i) any-neuron ::dt/weights count)))
+(defn- num-deltas [ctx] (-> ctx :delta/nn count))
+(defn- num-layers [ctx] (-> ctx :activation/nn count))
+(defn- num-layer-deltas [ctx i] (count (layer-deltas ctx i)))
+(defn- num-neurons-in-layer [{:keys [:activation/nn]} i] (-> nn (nth i) count))
+(defn- last-layer-index [ctx] (-> ctx num-layers dec))
+(defn- weights-from-neuron
+  "Returns a vector of weights *from* the jth neuron in the ith layer."
+  [ctx i j k]
+  (let [neuron-layer (layer ctx (inc i))
+        weights      (mapv #(get-in % [::dt/weights j]) neuron-layer)]
+    weights))
+(defn- computing-output-layer? [ctx i] (= (last-layer-index ctx) i))
+(defn- type-of-layer
+  [ctx i]
+  {:pre  [(<= 0 i (last-layer-index ctx))]}
+  (cond
+    (= i 0)                         ::input
+    (computing-output-layer? ctx i) ::output
+    :default                        ::hidden))
+
+(defn- error
+  "Measurement of the difference between a neurons output and it's goal."
+  [goal output]
+  (* 1/2 (square (- goal output))))
+
+(defmulti activation
+  "Calc the activation of a neuron.
+
+  Interesting to note that the activation function *must* be non-linear in order
+  for training to be possible (on non-linear data)."
+  (fn [type _] type))
+(defmulti dActivation
+  "Calc the derivative of the given activation fn with respect to it's input
+  i.e. the 'total-input'."
+  (fn [type _] type))
+
